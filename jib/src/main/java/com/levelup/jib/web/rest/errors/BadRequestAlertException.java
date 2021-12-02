91d303ce784838486381be25e7bbbff76098bf0e
saba <moslehsaba@gmail.com>
2021-12-02 11:32:43 +0330

    generate entities

---
+++ jib/src/main/java/com/levelup/jib/web/rest/errors/BadRequestAlertException.java
@@ -0,0 +1,6 @@
+package com.levelup.jib.web.rest.errors;
+
+public class BadRequestAlertException extends RuntimeException {
+    public BadRequestAlertException(String s, String entityName, String idexists) {
+    }
+}
