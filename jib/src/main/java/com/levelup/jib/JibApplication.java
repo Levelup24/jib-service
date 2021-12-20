aa0268e20c646b5379a3c6f76e3e1b77631001c5
kamalkarimi94 <kskarimi94@gmail.com>
2021-12-20 19:12:08 +0330

    Enable eureka client.

--- jib/src/main/java/com/levelup/jib/JibApplication.java
+++ jib/src/main/java/com/levelup/jib/JibApplication.java
@@ -2,8 +2,10 @@ package com.levelup.jib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JibApplication {
 
 	public static void main(String[] args) {
